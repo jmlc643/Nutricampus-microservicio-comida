5022738fe6b0505bde60f6102da6662a85c5b8de
jmlc643 <josemarialuyocampos@gmail.com>
2024-06-03 16:08:14 -0500

    first commit

---
+++ src/main/java/com/upao/pe/microserviciodieta/MicroservicioDietaApplication.java
@@ -0,0 +1,13 @@
+package com.upao.pe.microserviciodieta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioDietaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicioDietaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/upao/pe/microserviciodieta/MicroservicioDietaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upao.pe.microserviciodieta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioDietaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
