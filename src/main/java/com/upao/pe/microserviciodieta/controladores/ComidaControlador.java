daa1c027e5ba3f7d104fbbdc5b2a3f985b34fc64
jmlc643 <josemarialuyocampos@gmail.com>
2024-07-06 00:28:27 -0500

    Ultima prueba del auto deploy

--- src/main/java/com/upao/pe/microserviciodieta/controladores/ComidaControlador.java
+++ src/main/java/com/upao/pe/microserviciodieta/controladores/ComidaControlador.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class ComidaControlador {
     @Autowired private ComidaServicio comidaServicio;
 
-    @GetMapping("/listar/")
+    @GetMapping("/lista/")
     public List<Comida> listarComidas(){
         return comidaServicio.listarComidas();
     }
